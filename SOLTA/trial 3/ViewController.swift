b0638607e939907cc4a5bd6118a0cc5f13748abf
ansar-z-khan <ansark@hotmail.com>
2018-05-28 13:54:48 -0400

    Initial Commit

---
+++ SOLTA/trial 3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  trial 3
+//
+//  Created by Guest User on 2018-05-20.
+//  Copyright © 2018 Guest User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
