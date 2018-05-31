52f2c8937a6cd4da5132ad6868681764efbe364d
Unknown <ansark@hotmail.com>
2018-05-31 18:48:40 -0400

    Adds Support for the history page
    need to add stuff on story board.

---
+++ SOLTA/trial 3/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  trial 3
+//
+//  Created by Ansar Khan on 2018-05-31.
+//  Copyright © 2018 Guest User. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
